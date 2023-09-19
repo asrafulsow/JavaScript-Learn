e9afa50f0792ec363c578f3c66535ee87b927fb9
asrafulsow <aisowrav11@gmail.com>
2023-09-19 23:55:40 +0600

    whileLoop

--- while.js
+++ while.js
@@ -1,13 +1,13 @@
 // // 1 to 10 odd number find
-// let number = 0;
+let number = 0;
 
-// while (number < 20) {
-//     console.log(number);
-//     if (number % 2 == 1) {
-//         number = number + 1
-//     }
-//     number++
-// }
+while (number < 20) {
+    console.log(number);
+    if (number % 2 == 1) {
+        number = number + 1
+    }
+    number++
+}
 
 
 // start a simple while loop 
